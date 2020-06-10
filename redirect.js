8628e4a91ec816a37be45e1faf354f7d34efda21
Inv3nt0r1 <durgeshpachghare01@gmail.com>
2020-06-10 22:59:48 +0530

    Update link

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-var link = "https://www.google.com"
+var link = "https://26a33e77384b.in.ngrok.io"
 document.getElementById("redirect_button").onclick = function () {
     location.href = link;
 };
--- script.py
+++ script.py
@@ -1,12 +1,18 @@
-from pyngrok.conf import pyngrokConfig
+from pyngrok.conf import PyngrokConfig
 from pyngrok import ngrok
 import os
 
 import config
 
+print("Before auth setting")
 auth = config.auth
-ngrok.set_auto_token(auth)
+print("Auth: ",auth)
+#ngrok.set_auth_token(auth)
+pyngrok_config = PyngrokConfig(auth_token=auth)
+print("Auth set done")
 pyngrok_config = PyngrokConfig(region="in")
-
-public_url = ngrok.connect(80)
+print("region set done")
+#public_url = ngrok.connect()
+public_url = ngrok.connect(8096,pyngrok_config=pyngrok_config)
+print("connect is also done")
 print("URL: ",public_url)
