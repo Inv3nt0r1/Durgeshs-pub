a7b1f710205384d37187cc82b83eb55aaa3a2793
Inv3nt0r1 <durgeshpachghare01@gmail.com>
2020-06-11 15:56:27 +0530

    Update link

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-var link = "http://109145c42597.in.ngrok.io"
+var link = "http://886d3fce30aa.in.ngrok.io"
 document.getElementById("redirect_button").onclick = function () {
     location.href = link;
 };
