cf355ae9a77a1b041ff33eb79bdd9add482772e6
Inv3nt0r1 <durgeshpachghare01@gmail.com>
2020-06-11 18:18:55 +0530

    This commit is automatically done by the server script, to keep updating the reverse proxy tunnel

--- redirect.js
+++ redirect.js
@@ -1,2 +1,6 @@
-var link = "http://5c44222dde82.in.ngrok.io"
+var link = "http://9adbddd29088.in.ngrok.io"
+
+document.getElementById("redirect_button").onclick = function () {
+    location.href = link;
 };
+
--- script.py
+++ script.py
@@ -21,7 +21,7 @@ with open('redirect.js','r') as file:
     data = file.readlines()
 
 data[0] = 'var link = "'+public_url+'"\n'
-print(data[0])
+print(data)
 
 with open('redirect.js','w') as file:
     file.writelines(data)
