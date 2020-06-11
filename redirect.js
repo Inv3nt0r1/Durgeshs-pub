94533d4c093166150861bc539cc563d6ec1d89d7
Inv3nt0r1 <durgeshpachghare01@gmail.com>
2020-06-11 18:32:30 +0530

    git revert to useful commit

--- redirect.js
+++ redirect.js
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 var link = "http://ef401cbde146.in.ngrok.io"
     location.href = link;
 };
 
+=======
+var link = "http://886d3fce30aa.in.ngrok.io"
+document.getElementById("redirect_button").onclick = function () {
+    location.href = link;
+};
+>>>>>>> parent of 9c108e1... Test run: This commit is automatically done by the server script, to keep updating the reverse proxy tunnel
--- script.py
+++ script.py
@@ -6,7 +6,7 @@ import config
 
 print("Before auth setting")
 auth = config.auth
-#print("Auth: ",auth)
+print("Auth: ",auth)
 #ngrok.set_auth_token(auth)
 pyngrok_config = PyngrokConfig(auth_token=auth)
 print("Auth set done")
