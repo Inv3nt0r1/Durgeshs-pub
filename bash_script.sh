202bf8d6a92bd2f9bf5cb42c6906333e7c76e7f2
Inv3nt0r1 <durgeshpachghare01@gmail.com>
2020-06-13 23:10:42 +0530

    This commit is automatically done by the script, to change link of reverse proxy tunnel

--- bash_script.sh
+++ bash_script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 . /home/pi/.bashrc
 echo "Script loaded"
-#sudo mount /dev/sda1 /mnt/extdisk
+sudo mount /dev/sda1 /mnt/extdisk
 echo "Disk mounted"
 python3 /home/pi/Durgeshs-pub/script.py &
 echo "Python program started, sleeping for 30 seconds"
 sleep 30
 cd /home/pi/Durgeshs-pub/
+
 git add .
 git commit -m "This commit is automatically done by the script, to change link of reverse proxy tunnel"
+git pull
 git push
