fa9839da0f8eb49e4447eb2049c9b6ea80ea49a8
Inv3nt0r1 <durgeshpachghare01@gmail.com>
2020-06-17 21:50:38 +0530

    This commit is automatically done by the script, to change link of reverse proxy tunnel

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-var link = "http://d2589a7cc871.in.ngrok.io"
+var link = "http://cf68f917a115.in.ngrok.io"
 
 document.getElementById("redirect_button").onclick = function () {
     location.href = link;
