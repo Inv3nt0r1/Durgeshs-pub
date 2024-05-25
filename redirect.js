58d26941856ffe1fefac57f783f70b8a8600ab22
Durgesh Pachghare <dp185226@ncr.com>
2024-05-25 22:02:53 +0530

    Update logic to check if URL exist or not

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-var link = "https://fbaf-103-197-221-250.ngrok-free.app"
+var link = "https://fbaf-103-197-221-250.ngrok-free.app/web/"
 
 function sleep(milliseconds) {
     const date = Date.now();
@@ -9,17 +9,27 @@ function sleep(milliseconds) {
   }
 
   var timeout;
-  var timeout1;
+  var timeout1; 
   var timeout2;
-document.getElementById("redirect_button").onclick = function () {
-    var flag;
-    $("#more_info").html("Checking server status. Hang on..");
-    $.ajax({
-        url: link,
-        dataType: 'jsonp',
-        statusCode: {
-            302: function () {   
-                console.log("status code 200 returned. URL exist. Server is running."); 
+
+function urlExists(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('HEAD', url, true);
+    xhr.onreadystatechange = function() {
+        console.log(xhr);
+        if (xhr.readyState === 4) {
+            callback(xhr.status >= 200 && xhr.status < 300);
+        }
+    };
+    xhr.onerror = function() {
+        callback(false);
+    };
+    xhr.send();
+}
+document.getElementById('redirect_button').addEventListener('click', function() {
+    urlExists(link, function(exists) {
+        if(exists) {
+            console.log("status code 200 returned. URL exist. Server is running."); 
                 clearTimeout(timeout);
                 timeout = setTimeout(function() {
                     $("#more_info").html("Server is up and running.");
@@ -37,11 +47,10 @@ document.getElementById("redirect_button").onclick = function () {
 
                 clearTimeout(timeout2);
                 timeout2 = setTimeout(function() {
-                    location.href = link;
+                    window.location.href = link;
                 }, 5000);
-            },
-            404: function () {
-                console.log("status code 404 returned. URL does not exist. Server is not running.");
+        } else {
+            console.log("status code 404 returned. URL does not exist. Server is not running.");
                 clearTimeout(timeout);
                 timeout = setTimeout(function() {
                     $("#more_info").attr('style','color: red');
@@ -49,7 +58,7 @@ document.getElementById("redirect_button").onclick = function () {
                     document.getElementById("redirect_button").disabled = true;
                     document.getElementById("redirect_button").innerHTML = "Cannot redirect :(";
                 }, 2000);
-            }
         }
     });
-}
+});
+
