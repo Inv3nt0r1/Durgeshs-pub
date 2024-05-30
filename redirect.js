d5ab389dce884454927923d06bae16d7f26c6b0b
Durgesh Pachghare <durgeshpachghare01@gmail.com>
2024-05-30 20:36:23 +0530

    Update redirect.js - appended web in the URL

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-var link = "https://a4b1-103-197-221-248.ngrok-free.app"
+var link = "https://a4b1-103-197-221-248.ngrok-free.app/web/"
 
 function sleep(milliseconds) {
     const date = Date.now();
