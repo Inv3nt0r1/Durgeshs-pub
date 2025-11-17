52d59eb6f854c4e84278f84b0bec81325b462c34
Inv3nt0r1 <durgeshpachghare01@gmail.com>
2025-11-17 12:08:54 +0530

    Auto-update redirect link

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-var link = "https://expensive-panel-eagles-des.trycloudflare.com/web/"
+var link = "https://api.trycloudflare.com/web/"
 
 function sleep(milliseconds) {
     const date = Date.now();
