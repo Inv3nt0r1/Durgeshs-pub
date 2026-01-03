a32703b468a2a734cc250a47e4dedbd627e3d5db
Inv3nt0r1 <durgeshpachghare01@gmail.com>
2026-01-03 10:38:43 +0530

    Auto-update redirect link

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-var link = "https://minutes-convenience-exposure-couple.trycloudflare.com/web/"
+var link = "https://paris-creativity-learners-exotic.trycloudflare.com/web/"
 
 function sleep(milliseconds) {
     const date = Date.now();
